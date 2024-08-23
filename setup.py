f8d68aaf7904aa9b5899cf3484d6188a53b5ff0b
Yorick <yorickdowne@gmail.com>
2024-08-23 14:09:59 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+"""
+THIS IS A STUB FOR IMPORTING THE APP
+"""
+
+setup(
+    name="ethstaker_deposit",
+    version='0.1.2',
+    py_modules=["ethstaker_deposit"],
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires=">=3.9,<4",
+)
